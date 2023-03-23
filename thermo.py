5de7fee1b2783fae5cd6487a3ef2270cd5e3e102
DD4869 <110597693+DD4869@users.noreply.github.com>
2023-03-23 03:57:28 -0400

    Add files via upload

---
+++ thermo.py
@@ -0,0 +1,21 @@
+import Adafruit_DHT as DHT
+import requests
+
+##type of sensor
+SENSOR_TYPE = DHT.DHT22
+
+##GPI0 port number
+DHT_GPIO = 4
+
+## start mesurement
+h,t = DHT.read_retry(SENSOR_TYPE, DHT_GPIO)
+Temp = "Temp is {0:0.1f} ℃" .format(t)
+Humidity = "Humidity is = {0:0.1f} %" .format(h)
+message = "\n" + Temp + "\n" + Humidity
+print (message)
+
+url = "https://notify-api.line.me/api/notify"
+token = "Your LINE Notity Token"
+headers = {"Authorization" : "Bearer "+ token}
+payload = {"message" : message}
+r = requests.post(url, headers = headers, params = payload)
